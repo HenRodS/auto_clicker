2ae1a3ed473b48722036c2c8e4fe396361040f8d
HenRodS <henrique.karekas@gmail.com>
2026-02-27 19:39:14 -0300

    Consegue repetir o click ao segurar o botão (só keyboard)

--- auto_clicker.py
+++ auto_clicker.py
@@ -1,46 +1,51 @@
 import threading
 import time
-from pynput.keyboard import Key, Controller, Listener
+from pynput.keyboard import Key, Controller, Listener, KeyCode
 
 class AutoKey:
-    def __init__(self, key_to_press, toggle_key):
+    def __init__(self, key_to_repeat, toggle_key):
         self.keyboard = Controller()
-        self.key_to_press = key_to_press
+        self.key_to_repeat = KeyCode.from_char(key_to_repeat)
         self.toggle_key = toggle_key
-        self.running = False
+        self.is_pressing = False
         self.active = True
 
-    def toggle_action(self, key):
-        if key == self.toggle_key: 
-            self.running = not self.running
-            print(f"[STATUS] Repetição: {'LIGADA' if self.running else 'DESLIGADA'}")
+    def on_press(self, key):
+        """é chamada quando o listener detectou que soltou"""
+        if key == self.key_to_repeat:
+            self.is_pressing = True
 
-        if key == Key.alt_r:
+        if key == Key.alt_gr:
             self.active = False
-            self.running = False
             return False
         
+    def on_release(self, key):
+        """é chamada quando o listener detectar que soltou"""
+        if key == self.key_to_repeat:
+            self.is_pressing = False
+    
     def clicker(self):
         """Função que rodará em uma thread separada para não travar o teclado"""
         while self.active:
-            if self.running:
-                self.keyboard.press(self.key_to_press)
-                self.keyboard.release(self.key_to_press)
+            if self.is_pressing:
+                self.keyboard.press(self.key_to_repeat)
+                self.keyboard.release(self.key_to_repeat)
+                print("click!")
                 time.sleep(0.1)
             else:
                 time.sleep(0.1)
 
 
 if __name__ == "__main__":
-    app = AutoKey(key_to_press='e', toggle_key=Key.f8)
+    app = AutoKey(key_to_repeat='e', toggle_key=Key. f8)
 
     print("--- Script Iniciado ---")
     print("Aperte F8 para Ligar/Desligar")
-    print("Aperte ESC para encerrar o script")
+    print("Aperte Alt Gr para encerrar o script")
 
     # Cria uma thread para que o clicker rode em paralelo
     thread_clicker = threading.Thread(target=app.clicker)
     thread_clicker.start()
 
-    with Listener(on_press=app.toggle_action) as listener:
+    with Listener(on_press=app.on_press, on_release=app.on_release) as listener:
         listener.join()
